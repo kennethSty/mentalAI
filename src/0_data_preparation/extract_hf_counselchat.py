d5d36e222b02e6740e3c13531bcc9eb66e23f865
kennethSty <kenneth.styppa@web.de>
2025-02-07 15:24:55 +0100

    Make software design and plan milestones

---
+++ src/0_data_preparation/extract_hf_counselchat.py
@@ -0,0 +1,5 @@
+from datasets import load_dataset
+
+mental_counseling_ds = load_dataset("Amod/mental_health_counseling_conversations")
+counsel_chat_ds = load_dataset("nbertagnolli/counsel-chat")
+suicide_risk_ds = load_dataset("jquiros/suicide")
